c0e848e6ac7fb3051400f9dde39a8451804ab619
ColumboLAPD <54627695+ColumboLAPD@users.noreply.github.com>
2020-06-28 10:13:04 +0300

    test commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <QCoreApplication>
+#include <QTextStream>
+#include <iostream>
+#include <QDate>
+#include <QTime>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QTextStream out(stdout);
+    out.setCodec("CP866");
 
-    return a.exec();
+    QDate date1(2020, 6, 19);
+    out << date1.toString() << endl;
+
+    QDate cd = QDate::currentDate();
+    out << cd.toString() << endl;
+
+    return 0;
 }
